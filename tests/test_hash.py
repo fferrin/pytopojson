ef1410e034a58398f7ceb361fa8e6f5cb20069d5
Facundo Ferrin <facundo.ferrin@gmail.com>
2019-10-18 05:50:00 +0200

    Change hash function to pass hash tests

--- tests/test_hash.py
+++ tests/test_hash.py
@@ -1,8 +1,6 @@
 import unittest
 
-from topojson import bounds, delta, extract, geometry, prequantize
 from topojson.hash.hash import HashMap, HashSet
-from topojson.hash.point import hash as h, equal as e
 
 
 def equal(a, b):
@@ -10,32 +8,32 @@ def equal(a, b):
 
 
 def hash(o):
-    return o.hash
+    return o['hash']
 
 
 class HashMapTestCase(unittest.TestCase):
 
     def test_hashmap_can_get_an_object_by_key(self):
-        map = HashMap(10, h, equal)
+        map = HashMap(10, hash, equal)
         key = {'hash': 1}
         map.set(key, 42)
 
         self.assertEqual(map.get(key), 42)
 
     def test_hashmap_get_returns_undefined_when_no_key_is_found(self):
-        map = HashMap(10, h, equal)
+        map = HashMap(10, hash, equal)
         key = {'hash': 1}
 
         self.assertEqual(map.get(key), None)
 
     def test_hashmap_get_returns_the_missing_value_when_no_key_is_found(self):
-        map = HashMap(10, h, equal)
+        map = HashMap(10, hash, equal)
         key = {'hash': 1}
 
         self.assertEqual(map.get(key, 42), 42)
 
     def test_hashmap_when_a_hash_collision_occurs_get_checks_that_the_keys_are_equal(self):
-        map = HashMap(10, h, equal)
+        map = HashMap(10, hash, equal)
         key_1 = {'hash': 1}
         key_2 = {'hash': 1}
         key_3 = {'hash': 1}
@@ -48,7 +46,7 @@ class HashMapTestCase(unittest.TestCase):
         self.assertEqual(map.get(key_3), None)
 
     def test_hashmap_can_set_an_object_by_key(self):
-        map = HashMap(10, h, equal)
+        map = HashMap(10, hash, equal)
         key = {'hash': 1}
 
         map.set(key, 42)
@@ -56,7 +54,7 @@ class HashMapTestCase(unittest.TestCase):
         self.assertEqual(map.get(key), 42)
 
     def test_hashmap_can_set_an_object_by_key_if_not_already_set(self):
-        map = HashMap(10, h, equal)
+        map = HashMap(10, hash, equal)
         key = {'hash': 1}
 
         self.assertEqual(map.maybe_set(key, 42), 42)
@@ -65,14 +63,14 @@ class HashMapTestCase(unittest.TestCase):
         self.assertEqual(map.get(key), 42)
 
     def test_hashmap_set_returns_the_set_value(self):
-        map = HashMap(10, h, equal)
+        map = HashMap(10, hash, equal)
         key = {'hash': 1}
 
         self.assertEqual(map.set(key, 42), 42)
 
     def test_hashmap_set_throws_an_error_when_full(self):
         # minimum size of 16
-        map = HashMap(0, h, equal)
+        map = HashMap(0, hash, equal)
         keys = list()
 
         for i in range(16):
@@ -87,7 +85,7 @@ class HashMapTestCase(unittest.TestCase):
             map.set({'hash': 16}, True)
 
     def test_hashmap_when_a_hash_collision_occurs_set_checks_that_the_keys_are_equal(self):
-        map = HashMap(10, h, equal)
+        map = HashMap(10, hash, equal)
         key_1 = {'hash': 1}
         key_2 = {'hash': 1}
         key_3 = {'hash': 1}
@@ -100,7 +98,7 @@ class HashMapTestCase(unittest.TestCase):
         self.assertEqual(map.get(key_3), None)
 
     def test_hashmap_the_hash_function_must_return_a_nonnegative_integer_but_can_be_greater_than_size(self):
-        map = HashMap(10, h, equal)
+        map = HashMap(10, hash, equal)
         key = {'hash': 11}
 
         self.assertEqual(map.get(key), None)
@@ -111,20 +109,20 @@ class HashMapTestCase(unittest.TestCase):
 class HashSetTestCase(unittest.TestCase):
 
     def test_hashset_can_get_an_object(self):
-        map = HashSet(10, h, equal)
+        map = HashSet(10, hash, equal)
         key = {'hash': 1}
 
         self.assertEqual(map.add(key), True)
         self.assertEqual(map.has(key), True)
 
     def test_hashset_has_returns_false_when_no_key_is_found(self):
-        map = HashSet(10, h, equal)
+        map = HashSet(10, hash, equal)
         key = {'hash': 1}
 
         self.assertEqual(map.has(key), False)
 
     def test_hashset_when_a_hash_collision_occurs_get_checks_that_the_keys_are_equal(self):
-        map = HashSet(10, h, equal)
+        map = HashSet(10, hash, equal)
         key_1 = {'hash': 1}
         key_2 = {'hash': 1}
         key_3 = {'hash': 1}
@@ -136,14 +134,14 @@ class HashSetTestCase(unittest.TestCase):
         self.assertEqual(map.has(key_3), False)
 
     def test_hashset_add_returns_true(self):
-        map = HashSet(10, h, equal)
+        map = HashSet(10, hash, equal)
         key = {'hash': 1}
 
         self.assertEqual(map.add(key), True)
 
     def test_hashset_add_throws_an_error_when_full(self):
         # minimum size of 16
-        map = HashSet(0, h, equal)
+        map = HashSet(0, hash, equal)
         keys = list()
 
         for i in range(16):
@@ -158,7 +156,7 @@ class HashSetTestCase(unittest.TestCase):
             map.add({'hash': 16})
 
     def test_hashset_the_hash_function_must_return_a_nonnegative_integer_but_can_be_greater_than_size(self):
-        map = HashSet(10, h, equal)
+        map = HashSet(10, hash, equal)
         key = {'hash': 11}
 
         self.assertEqual(map.has(key), False)
