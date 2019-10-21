74ddd054dff210acbb00ad04d14c14b4b2186d28
Facundo Ferrin <facundo.ferrin@gmail.com>
2019-10-21 05:00:46 +0200

    Add files to upload project to PyPi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytopojson",
+    version="0.0.1",
+    author="Facundo Ferrín",
+    author_email="facundo.ferrin@gmail.com",
+    description="An extension to GeoJSON that encodes topology",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fferrin/pytopojson",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7.3',
+)
