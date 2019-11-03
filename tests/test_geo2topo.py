c7e675d1e75849fc46a8567334d47d620738993f
Facundo Ferrin <facundo.ferrin@gmail.com>
2019-11-03 07:07:11 +0100

    Add tests for geo2topo

---
+++ tests/test_geo2topo.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+import json
+import subprocess
+import unittest
+
+# Third-party imports
+
+# Application-specific imports
+from tests import in_delta
+
+
+class Geo2TopoTestCase(unittest.TestCase):
+    def geo2topo(self, output, options=''):
+        options = ' '.join(options.split()).split(' ')
+        p = subprocess.run(['python', '../bin/geo2topo.py', *options],
+                           capture_output=True)
+        out, err = p.stdout, p.stderr
+        actual = json.loads(out.decode('utf8').strip().replace("'", '"'))
+        with open(output, 'r') as f:
+            expected = json.load(f)
+
+        self.assertTrue(in_delta.in_delta(actual, expected))
+
+    def test_polygons(self):
+        self.geo2topo('topojson/polygon-no-quantization.json',
+                      'polygon=geojson/polygon-clockwise.json')
+
+    def test_quantized_polygons(self):
+        self.geo2topo('topojson/polygon.json',
+                      '-q 1E4 polygon=geojson/polygon-clockwise.json')
+
+    def test_empty_geometries(self):
+        self.geo2topo('topojson/empty.json',
+                      'multilinestring=geojson/empty-multilinestring.json'
+                      ' multipoint=geojson/empty-multipoint.json'
+                      ' multipolygon=geojson/empty-multipolygon.json'
+                      ' multipolygon2=geojson/empty-multipolygon2.json'
+                      ' polygon=geojson/empty-polygon.json')
