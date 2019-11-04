86cf8beb8f585e4ba8b15174a366cd05ec89ec58
Facundo Ferrin <facundo.ferrin@gmail.com>
2019-11-04 04:17:12 +0100

    Update Python version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytopojson",
-    version="0.0.1",
+    version="0.0.2",
     author="Facundo Ferrín",
     author_email="facundo.ferrin@gmail.com",
     description="An extension to GeoJSON that encodes topology",
@@ -14,9 +14,12 @@ setuptools.setup(
     url="https://github.com/fferrin/pytopojson",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+      'numpy >= 1.15.0',
     ],
     python_requires='>=3.7.3',
 )
