85bc997ecc6ce0f2d1ba1b696bad72af437a82a9
Facundo Ferrin <facundo.ferrin@gmail.com>
2019-11-04 04:16:58 +0100

    Fix path of input files

--- tests/test_geo2topo.py
+++ tests/test_geo2topo.py
@@ -14,7 +14,7 @@ from tests import in_delta
 class Geo2TopoTestCase(unittest.TestCase):
     def geo2topo(self, output, options=''):
         options = ' '.join(options.split()).split(' ')
-        p = subprocess.run(['python', '../bin/geo2topo.py', *options],
+        p = subprocess.run(['python', 'bin/geo2topo.py', *options],
                            capture_output=True)
         out, err = p.stdout, p.stderr
         actual = json.loads(out.decode('utf8').strip().replace("'", '"'))
@@ -24,17 +24,17 @@ class Geo2TopoTestCase(unittest.TestCase):
         self.assertTrue(in_delta.in_delta(actual, expected))
 
     def test_polygons(self):
-        self.geo2topo('topojson/polygon-no-quantization.json',
-                      'polygon=geojson/polygon-clockwise.json')
+        self.geo2topo('tests/topojson/polygon-no-quantization.json',
+                      'polygon=tests/geojson/polygon-clockwise.json')
 
     def test_quantized_polygons(self):
-        self.geo2topo('topojson/polygon.json',
-                      '-q 1E4 polygon=geojson/polygon-clockwise.json')
+        self.geo2topo('tests/topojson/polygon.json',
+                      '-q 1E4 polygon=tests/geojson/polygon-clockwise.json')
 
     def test_empty_geometries(self):
-        self.geo2topo('topojson/empty.json',
-                      'multilinestring=geojson/empty-multilinestring.json'
-                      ' multipoint=geojson/empty-multipoint.json'
-                      ' multipolygon=geojson/empty-multipolygon.json'
-                      ' multipolygon2=geojson/empty-multipolygon2.json'
-                      ' polygon=geojson/empty-polygon.json')
+        self.geo2topo('tests/topojson/empty.json',
+                      'multilinestring=tests/geojson/empty-multilinestring.json'
+                      ' multipoint=tests/geojson/empty-multipoint.json'
+                      ' multipolygon=tests/geojson/empty-multipolygon.json'
+                      ' multipolygon2=tests/geojson/empty-multipolygon2.json'
+                      ' polygon=tests/geojson/empty-polygon.json')
