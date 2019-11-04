bff5e28ff4b82c05f8007c7ca712dc36c4f8e9b0
Facundo Ferrin <facundo.ferrin@gmail.com>
2019-11-04 04:17:12 +0100

    Update Python version in setup.py

--- setup.py
+++ setup.py
@@ -18,5 +18,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+      'numpy >= 1.15.0',
+    ],
     python_requires='>=3.7.3',
 )
