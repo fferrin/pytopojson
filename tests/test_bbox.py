b8afeea141e99edf043580a0e3676a4190305c58
Facundo Ferrin <facundo.ferrin@gmail.com>
2019-11-28 05:50:12 +0100

    Add tests for BBox class. Test passing

---
+++ tests/test_bbox.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+import json
+import math
+import unittest
+
+# Third-party imports
+
+# Application-specific imports
+from pytopojson import bbox
+
+
+class BBoxTestCase(unittest.TestCase):
+    def setUp(self):
+        self.bbox = bbox.BBox()
+
+    @staticmethod
+    def _load_json_file(filepath):
+        with open(filepath, 'r') as src:
+            return json.load(src)
+
+    def test_bbox_topology_ignores_the_existing_bbox_if_any(self):
+        bbox = [1, 2, 3, 4]
+        self.assertListEqual(
+            self.bbox({
+                'type': 'Topology',
+                'bbox': bbox,
+                'objects': {},
+                'arcs': {}
+            }),
+            [math.inf, math.inf, -math.inf, -math.inf]
+        )
+
+    def test_bbox_topology_computes_the_bbox_for_a_quantized_topology_if_missing(self):
+        topology = self._load_json_file('tests/topojson/polygon-q1e4.json')
+        self.assertListEqual(self.bbox(topology), [0, 0, 10, 10])
+
+    def test_bbox_topology_computes_the_bbox_for_a_non_quantized_topology_if_missing(self):
+        topology = self._load_json_file('tests/topojson/polygon-no-quantization.json')
+        self.assertListEqual(self.bbox(topology), [0, 0, 10, 10])
+
+    def test_bbox_topology_considers_points(self):
+        topology = self._load_json_file('tests/topojson/point.json')
+        self.assertListEqual(self.bbox(topology), [0, 0, 10, 10])
+
+    def test_bbox_topology_considers_multipoints(self):
+        topology = self._load_json_file('tests/topojson/points.json')
+        self.assertListEqual(self.bbox(topology), [0, 0, 10, 10])
