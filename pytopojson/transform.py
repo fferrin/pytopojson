282676ab0f2a50c5f5efadd7d757c0220c46df09
Facundo Ferrin <facundo.ferrin@gmail.com>
2019-11-28 05:27:57 +0100

    Add Transform and Untransform classes

---
+++ pytopojson/transform.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+
+# Third-party imports
+
+# Application-specific imports
+from pytopojson import identity
+
+
+class Transform(object):
+    def __init__(self):
+        self.identity = identity.Identity()
+        self.x_0, self.y_0 = 0, 0
+        self.k_x, self.k_y = 0, 0
+        self.d_x, self.d_y = 0, 0
+
+    def __call__(self, transform=None, *args, **kwargs):
+        # self.x_0, self.y_0 = 0, 0
+        # self.k_x, self.k_y = 0, 0
+        # self.d_x, self.d_y = 0, 0
+        if transform is None:
+            return self.identity
+
+        self.k_x, self.k_y = transform['scale']
+        self.d_x, self.d_y = transform['translate']
+
+        return self.func
+
+    def func(self, input, i=None):
+        if i is None or i == 0:
+            self.x_0, self.y_0 = 0, 0
+
+        output = input.copy()
+
+        self.x_0 += input[0]
+        self.y_0 += input[1]
+        output[0] = self.x_0 * self.k_x + self.d_x
+        output[1] = self.y_0 * self.k_y + self.d_y
+
+        return output
---
+++ pytopojson/untransform.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+
+# Third-party imports
+
+# Application-specific imports
+from pytopojson import identity
+
+
+class Untransform(object):
+    def __init__(self):
+        self.identity = identity.Identity()
+        self.x_0 = 0
+        self.y_0 = 0
+        self.k_x = 0
+        self.k_y = 0
+        self.d_x = 0
+        self.d_y = 0
+
+    def __call__(self, transform=None, *args, **kwargs):
+        if transform is None:
+            return self.identity
+
+        self.k_x, self.k_y = transform['scale']
+        self.d_x, self.d_y = transform['translate']
+
+        return self.func
+
+    def func(self, input, i=None):
+        if i is None or i == 0:
+            self.x_0, self.y_0 = 0, 0
+        output = input.copy()
+
+        x_1 = int(round((input[0] - self.d_x) / self.k_x))
+        y_1 = int(round((input[1] - self.d_y) / self.k_y))
+        output[0] = x_1 - self.x_0
+        output[1] = y_1 - self.y_0
+        self.x_0 = x_1
+        self.y_0 = y_1
+
+        return output
